8663abe514cfec13dae5665a7218356fc201d0b7
nagirinyaphionaladeen <phionaladeen@gmail.com>
2017-05-17 12:50:23 +0300

    Update progamming work.py

--- progamming work.py
+++ progamming work.py
@@ -1,9 +1,9 @@
-
+NAGIRINYA PHIONAH BIOMEDICAL ENGINEERING 16/U/8330/ps
 import datetime
 years =float(input('Enter your age:'))
 month=int(input('Enter month of birth:'))
 day=int(input('Date of birth:'))
-days_per_year=365.24
+days_per_year=365.24ps
 birthdate =datetime.datetime.now()  - datetime.timedelta(days=(years*days_per_year))
 y=int(birthdate.strftime("%Y"))
 x=datetime.date(y,month,day)
